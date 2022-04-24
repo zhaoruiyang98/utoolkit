a68084978ccf681a0bcce1d9e8b098431d66381d
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2022-04-24 23:46:22 +0800

    refact(cover)

---
+++ cosmotoolkit/cover.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from pathlib import Path
+from PIL import Image
+from cosmotoolkit.log import HasLogger
+from cosmotoolkit.log import LoggedError
+
+
+@dataclass
+class Cover(HasLogger):
+    file: str
+    output: str = "cover.jpg"
+    force: bool = False
+    extension: str = "jpeg"
+
+    def __post_init__(self):
+        self.set_logger()
+        output = Path(self.output)
+        if output.exists() and not self.force:
+            raise LoggedError(self.log, "%s already exists", self.output)
+
+    def load(self) -> Image.Image:
+        try:
+            with Image.open(self.file) as image:
+                image.convert('RGB')
+        except OSError as err:
+            raise LoggedError(
+                self.log, "%s is not a valid image file", self.file) from err
+        return image
+
+    def resize(self, refwidth: int = 1146, refheight: int = 717) -> Image.Image:
+        image = self.load()
+
+        width, height = image.size
+        ratio = width / height
+        cwidth, cheight = refwidth / width, refheight / height
+        if cwidth > cheight:
+            newsize = (int(width * cwidth), int(width * cwidth / ratio))
+        else:
+            newsize = (int(height * cheight * ratio), int(height * cheight))
+        image = image.resize(newsize, Image.LANCZOS)
+
+        return image
+
+    def run(self) -> None:
+        image = self.resize()
+        image.save(self.output, self.extension)
--- cosmotoolkit/script.py
+++ cosmotoolkit/script.py
@@ -1,73 +1,15 @@
+from __future__ import annotations
 import argparse
 import logging
 import sys
-from pathlib import Path
-from PIL import Image
+from argparse import Namespace
+from typing import cast
+from cosmotoolkit.cover import Cover
+from cosmotoolkit.log import LoggedError
+from cosmotoolkit.log import setup_logging
 
 
-class CustomFormatter(logging.Formatter):
-    """Logging colored formatter, adapted from https://stackoverflow.com/a/56944256/3638629"""
-
-    grey = '\x1b[38;21m'
-    blue = '\x1b[38;5;39m'
-    yellow = '\x1b[38;5;226m'
-    red = '\x1b[38;5;196m'
-    bold_red = '\x1b[31;1m'
-    reset = '\x1b[0m'
-
-    def __init__(self, fmt: str = "%(levelname)8s: %(message)s"):
-        super().__init__()
-        self.fmt = fmt
-        self.FORMATS = {
-            logging.DEBUG: self.grey + self.fmt + self.reset,
-            logging.INFO: self.blue + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.bold_red + self.fmt + self.reset
-        }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
-class CoverNameSpace(argparse.Namespace):
-    file: str
-    output: str
-    force: bool
-    extension: str
-
-
-def run_cover(args: CoverNameSpace, logger):
-    try:
-        with Image.open(args.file) as image:
-            image.convert('RGB')
-    except OSError:
-        logger.error(f"{args.file} is not a valid image file")
-        sys.exit(1)
-
-    width, height = image.size
-    ratio = width / height
-    cwidth, cheight = 1146 / width, 717 / height
-    if cwidth > cheight:
-        newsize = (int(width * cwidth), int(width * cwidth / ratio))
-    else:
-        newsize = (int(height * cheight * ratio), int(height * cheight))
-    image = image.resize(newsize, Image.LANCZOS)
-
-    output = Path(args.output)
-    if output.exists() and not args.force:
-        logger.error("%s already exists", output)
-        sys.exit(1)
-    image.save(args.output, args.extension)
-
-
-def run_sub(args, logger):
-    pass
-
-
-def main():
+def get_parser():
     parser = argparse.ArgumentParser(description='cosmotoolkit')
     parser.add_argument('-v', '--verbose', action='count', default=0)
     subparsers = parser.add_subparsers(
@@ -88,30 +30,49 @@ def main():
     sub_parser = subparsers.add_parser("sub", help="process subtitles")
     sub_parser.add_argument("file", help="input file path")
     sub_parser.add_argument(
-        "-o", "--output", help="output file path", default="sub.ass")
+        "-o", "--output", help="output file path", default="")
+    sub_parser.add_argument(
+        "-f", "--force", help="force overwrite", action="store_true")
 
+    return parser
+
+
+class CoverNameSpace(Namespace):
+    file: str
+    output: str
+    force: bool
+    extension: str
+
+
+class SubNameSpace(Namespace):
+    file: str
+    output: str
+    force: bool
+
+
+def main():
+    parser = get_parser()
     args = parser.parse_args()
 
-    logger = logging.getLogger(__name__)
-    stdout_handler = logging.StreamHandler()
-    stdout_handler.setFormatter(CustomFormatter())
     if args.verbose == 0:
-        logger.setLevel(logging.WARNING)
-        stdout_handler.setLevel(logging.WARNING)
+        level = logging.WARNING
     elif args.verbose == 1:
-        logger.setLevel(logging.INFO)
-        stdout_handler.setLevel(logging.INFO)
+        level = logging.INFO
     else:
-        logger.setLevel(logging.DEBUG)
-        stdout_handler.setLevel(logging.DEBUG)
-    logger.addHandler(stdout_handler)
+        level = logging.DEBUG
+    setup_logging(level)
 
-    if args.subcommand == "cover":
-        run_cover(args, logger)  # type: ignore
-    elif args.subcommand == "sub":
-        run_sub(args, logger)  # type: ignore
-    else:
-        parser.print_help()
+    try:
+        if args.subcommand == "cover":
+            args = cast(CoverNameSpace, args)
+            Cover(args.file, args.output, args.force, args.extension).run()
+        elif args.subcommand == "sub":
+            args = cast(SubNameSpace, args)
+            pass
+        else:
+            parser.print_help()
+    except LoggedError:
+        sys.exit(1)
 
 
 if __name__ == "__main__":
